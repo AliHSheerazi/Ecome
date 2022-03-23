c433572c070c6fa4959a13ec0299f5a2598355e2
AliHSheerazi <alisheerazi178@gmail.com>
2022-03-23 15:26:42 +0500

    checkout page

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -44,5 +45,51 @@ class ProductController extends Controller
         $product->meta_description = $request->input('meta_description');
         $product->save();
         return redirect('/products')->with('status' , "Product Added Successfully");
+    }
+    public function edit($id){
+        $product = Product::find($id);
+        return view('admin.products.edit', compact('product')); 
+    }
+
+    public function update($id, Request $request){
+        $product = Product::find($id);
+        if($request->hasFile('image')){
+            $path = 'assets/uploads/products/'.$product->image;
+            if(File::exists($path)){
+                File::delete($path);
+            }
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time(). '.'. $ext;
+            $file->move('assets/uploads/products',$filename);
+            $product->image = $filename;
+        }
+        $product->name = $request->input('name');
+        $product->slug = $request->input('slug');
+        $product->small_description = $request->input('small_description');
+        $product->description = $request->input('description');
+        $product->original_price = $request->input('original_price');
+        $product->selling_price = $request->input('selling_price');
+        $product->qty = $request->input('qty');
+        $product->tax = $request->input('tax');
+        $product->status = $request->input('status') == true?'1':'0';
+        $product->trending = $request->input('trending') == true?'1':'0';
+        $product->meta_title = $request->input('meta_title');
+        $product->meta_keywords = $request->input('meta_keywords');
+        $product->meta_description = $request->input('meta_description');
+        $product->update();
+        return redirect('dashboard')->with('status', 'Product Updated Siccessfully');
+    }
+
+    public function destroy(Request $request, $id){
+        $product = Product::find($id);
+        if($request->hasFile('image')){
+            $path = 'assets/uploads/products/'.$product->image;
+            if(File::exists($path)){
+                File::delete($path);
+            }
+        }
+        $product->delete();
+        return redirect('products')->with('status','Product Deleted Successfully');
     }
 }
